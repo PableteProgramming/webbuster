80b23f6dadd59878c1357593872125d2b384cd07
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2021-11-03 20:23:11 +0100

    Getting inputed file content

--- js/index.js
+++ js/index.js
@@ -2,7 +2,16 @@ $(function(){
 	$("body").hide().fadeIn(1000);
 })
 
+$(document).ready(function () {
+	bsCustomFileInput.init()
+})
+
 function Search(){
-	let content= document.getElementById("url-form").value;
-	alert(content);
+	var fr=new FileReader();
+	let input= document.getElementById("inputGroupFile04");
+	fr.readAsText(input.files[0])
+	fr.onload = function(e) {
+ 		let content= fr.result;
+		//content is now the file content
+	};;
 }
